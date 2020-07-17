388d685eb5f200e4f7d2a8933b2e04182e2b0f6d
dinka-vedelka <flak22@ukr.net>
2020-07-17 20:13:38 +0300

    added person class

--- src/com/itstep/Main.java
+++ src/com/itstep/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("123abc");
+        Person person = new Person("Camilla");
+        System.out.println(person);
     }
 }
